4cb148b40e2d53a5dc460bc07940e538a38c40da
svanlan1 <endoflineprod@gmail.com>
2016-02-16 13:59:44 -0600

    Big commit
    Added 'Notes' dialog when right clicking a flag
    All flags are now draggable, in case the user isn't happy with the
    current position of the flag.
    Added z-index stuff so the flags are always on top.
    Changed the welcome text on the top side of the <iframe>

--- js/marker.js
+++ js/marker.js
@@ -1,23 +1,44 @@
-var mif;
-var mifBody;
-var mCount = 1;
+/**************************************************
+*	Filename: 	Marker.js
+*	Author: 	Shea VanLaningham
+*	Website: 	https://github.com/svanlan1/Marker
+*
+***************************************************/
+
+//Declare global variables
+var mif; // Marker iframe
+var mifBody; //Marker iframe body element
+var mCount = 1; // Running tally of how many flags have been placed on the page
+
+/***********************************************************************************************************
+*	run_marker() function sets up the iframe on the left and sets the original body element positioned right
+***********************************************************************************************************/
 function run_marker() {
+	//Send message to background script letting it know that the application is currently active.
 	chrome.runtime.sendMessage({
 		greeting: 'start'
 	});
+
+	//Variable controls whether or not clicking in the page should place a flag.  Default = false
 	localStorage.setItem('marker_place_flag', 'false');
-	$('body').wrap('<div class="marker_body_wrap" />').css({
-		'width': $(window).width() - 300 + 'px'
-	});
-	var marker_div_container = $('<div />').attr('id', 'marker_div_container').prependTo('body');
+
+	//Wrap the original body element in a <DIV> and set the width appropriately
+	var width = $(window).width() - 300 + 'px';
+	$('body').wrapInner('<div class="marker_body_wrap" style="width:'+width+'" />');
+	$('body').prepend('<div class="shim"></div>')
+
+	//var marker_div_container = $('<div />').attr('id', 'marker_div_container').before('body');
+	//Create the MARKER iframe and append it to the <HTML> element
 	$('<iframe />').attr({
 		'id': 'marker_iframe',
 		'class': 'marker'
-	}).appendTo(marker_div_container);
+	}).appendTo('html');
 
+	//Set the global variables to their correct values
 	mif = $("#marker_iframe")[0].contentWindow.document;
 	mifBody = $(mif).find('body');
 
+	//Setting up <iframe> with CSS files and welcome text
 	$('<link />').attr({
 		'rel': 'stylesheet',
 		'type': 'text/css',
@@ -30,11 +51,38 @@ function run_marker() {
 		'href': 'https://fonts.googleapis.com/css?family=Raleway'
 	}).appendTo($(mif).find('head'));
 
-	$('<h1 />').attr('class', 'marker').html('Marker <img src="' + chrome.extension.getURL('images/marker_32.png') + '" alt="" />').appendTo(mifBody);
-	$('<span />').attr({
-		'class': 'marker instruction'
-	}).text('Select an option below to begin').appendTo(mifBody);
+	$('<link />').attr({
+		'rel': 'stylesheet',
+		'type': 'text/css',
+		'href': 'https://fonts.googleapis.com/css?family=Raleway'
+	}).appendTo('head');	
 
+	$('<h1 />').attr('class', 'marker').html('Marker <img src="' + chrome.extension.getURL('images/marker_32.png') + '" alt="" />').appendTo(mifBody);
+	$('<div />').attr({
+		'class': 'marker welcome'
+	}).text('a tool for making accessibility recommendations').appendTo(mifBody);
+	$('<div />').attr({
+		'class': 'marker welcome'
+	}).text('To begin, select an option below to either place a flag or select an area that you\'d like to confine your annotations to.').appendTo(mifBody);	
+	$('<div />').attr({
+		'class': 'marker welcome'
+	}).text('Once you place a flag, you will be able to choose what type of element you think it should be').appendTo(mifBody);
+
+	$('<div />').attr({
+		'class': 'marker welcome'
+	}).text('To add notes to a flag, simply right click on it.').appendTo(mifBody);	
+
+	//getStarted() will draw the rest of the contents in the <iframe>.  Separating them to keep functions small and light.
+	getStarted();
+
+}
+
+/******************************************************************************
+* getStarted() function draws the remaining startup UI elements in the <iframe>
+*******************************************************************************/
+function getStarted() {
+	
+	//Create <div> container for the actionable elements
 	var marker_options_div = $('<div />').attr('id', 'marker_options').appendTo(mifBody);
 
 	$('<a />').attr({
@@ -54,12 +102,12 @@ function run_marker() {
 
 	}).appendTo(marker_options_div);
 
-	/*$('<a />').attr({
+	$('<a />').attr({
 		'href': 'javascript:void(0);',
 		'id': 'marker_draws',
 		'class': 'marker_option'
-	}).html('<img src="' + chrome.extension.getURL('images/select_32.png') + '" alt="Click to select a section of the page" />').appendTo(marker_options_div);	
-*/
+	}).html('<img src="' + chrome.extension.getURL('images/select_24.png') + '" alt="Click to select a section of the page" />').appendTo(marker_options_div);	
+
 	$('<a />').attr({
 		'href': 'javascript:void(0);',
 		'id': 'marker_saves',
@@ -73,27 +121,31 @@ function run_marker() {
 	$('<div />').addClass('screen-reader-only').html('<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>').appendTo(mifBody);
 	$('<div />').attr('class', 'screen-reader-only').html('<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>').appendTo(mifBody);
 	$('<div />').addClass('screen-reader-only').html('<div>Icons made by <a href="http://www.flaticon.com/authors/nice-and-serious" title="Nice and Serious">Nice and Serious</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>').appendTo(mifBody);
+	$('<div />').addClass('screen-reader-only').html('<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>').appendTo(mifBody);
+	$('<div />').addClass('screen-reader-only').html('<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>').appendTo(mifBody);
 }
+
 
 function place_marker() {
 	$('.marker_body_wrap').css('cursor', 'pointer');
-	$('.marker_body_wrap, .marker_body_wrap a, .marker_body_wrap button').bind('click', function(e) {
-		var x = e.pageX - 310,
+	$('.marker_body_wrap, a, button').bind('click', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		var x = e.pageX - 10,
 			y = e.pageY - 15;
-
-		console.log('X: ' + x + ', Y:' + y);
 
 		var flag_wrap = $('<a />').attr('href', 'javascript:void(0);').attr('class', 'marker_page_marker').attr('data-marker-count', mCount).css({
 			'position': 'absolute',
 			'left': x + 'px',
 			'top': y + 'px'
+		}).bind('contextmenu', function(e) {
+			e.preventDefault();
+			createContextMenu($(this), e);
 		}).click(function(e) {
 			e.preventDefault();
 			$(mifBody).find('#marker_select_box_' + $(this).attr('data-marker-count')).focus();
-			
-
 			return false;
-		}).appendTo('body');
+		}).appendTo('body').draggable();
 		var m = $('<img />').attr({
 			'src': chrome.extension.getURL('images/flag_24.png'),
 			'class': 'marker_page_marker',
@@ -109,6 +161,48 @@ function place_marker() {
 
 function unplace_marker() {
 	$('.marker_body_wrap, .marker_body_wrap a, .marker_body_wrap button').unbind('click');
+}
+
+function createContextMenu(el, e) {
+	var id = 'marker_context_menu' + $(el).attr('data-marker-count');
+	if($('#' + id).length === 0) {
+		var menu = $('<div />').addClass('marker_context_menu').css({
+			'left': e.pageX + 35 + 'px',
+			'top': $(el).css('top')
+		}).attr('role', 'dialog').attr('id', id).appendTo('body');
+
+		var close = $('<a />').attr({
+			'class': 'marker_context_close',
+			'href': 'javascript:void(0)'
+		}).html('<span class="screen-reader-only">Close context menu for number' + $(el).attr('data-marker-count') + '</span><img src="'+chrome.extension.getURL('images/close.png')+'" alt="" />').appendTo(menu);
+
+		$(close).click(function(e) {
+			hideMenu('marker_context_menu' + $(el).attr('data-marker-count'));
+		});
+
+		$('<label />').addClass('marker').attr('for', 'marker_textarea_' + $(el).attr('data-marker-count')).text('Add notes').appendTo(menu);
+		$('<textarea />').addClass('marker marker_note').attr('id', 'marker_textarea_' + $(el).attr('data-marker-count')).appendTo(menu);
+		$('<button />').addClass('marker marker_fun_btn').attr('value', 'Save').text('Save').click(function(e) {
+			var text = $('#marker_textarea_' + $(el).attr('data-marker-count')).val(),
+				side_box = $(mifBody).find('#marker_select_box_' + $(el).attr('data-marker-count')).parent();
+			if($(side_box).find('.marker_user_note_side').length > 0) {
+				$(side_box).find('.marker_user_note_side').remove();
+			}
+			$(side_box).append('<div class="marker marker_user_note_side"><strong>User note:</strong><br />' + text + '</div>');
+
+		}).appendTo(menu);
+	} else {
+		showMenu(id);
+	}
+
+}
+
+function hideMenu(id) {
+	$('#' + id).hide();
+}
+
+function showMenu(id) {
+	$('#' + id).show();
 }
 
 function add_marker_text() {
@@ -136,10 +230,14 @@ function add_marker_select_options(divItem) {
 		}
 	});
 
-	var sel = $('<select />').attr('id', 'marker_select_box_' + mCount).appendTo(divItem);
+	var sel = $('<select />').attr('id', 'marker_select_box_' + mCount).attr('aria-required', 'true').appendTo(divItem);
 	$(options).each(function(i,v) {
 		$('<option />').attr('value', v.Value).text(v.QuickName).appendTo(sel);
 	});
+}
+
+function displayContextMenu() {
+	console.log('test');
 }
 
 function stop_marker() {
